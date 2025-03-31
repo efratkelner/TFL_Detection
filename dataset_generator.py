9ef8c525d3bf6cca2be0268b8cf2150123d38a9b
Efrat Kelner <efratkelner@gmail.com>
2025-03-31 12:44:56 +0300

    Improve traffic light dataset generation

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,5 +1,5 @@
 from random import random
-
+import random
 try:
     import os
     import json
@@ -8,17 +8,15 @@ try:
     import cv2
     import numpy as np
     import matplotlib.pyplot as plt
-    from shapely.geometry import Point
-    from shapely.geometry.polygon import Polygon
     from scipy import signal as sg
     from scipy.ndimage.filters import maximum_filter
     from PIL import Image
+    from shapely.geometry import Point
+    from shapely.geometry.polygon import Polygon
 
 except ImportError as e:
     print(f"Installation error: {e}")
     raise
-
-from PIL import Image
 
 plt.rcParams.update({'figure.max_open_warning': 0})
 
@@ -35,10 +33,10 @@ def crop_image(x, y, img, tav):
     im = Image.open(img)
     plt.imshow(im)
 
-    left = x - (81 / 2)
-    top = y - (81 / 2)
-    right = x + (81 / 2)
-    bottom = y + (81 / 2)
+    left = x - (81/2)
+    top = y - (81/2)
+    right = x + (81/2)
+    bottom = y + (81/2)
 
     try:
         im = im.crop((left, top, right, bottom))
@@ -49,18 +47,19 @@ def crop_image(x, y, img, tav):
             im.save(not_tfl_path + '\\' + tav + str(not_tfl_counter) + ".png")
             not_tfl_counter += 1
         plt.cla()
+
     except:
         print("Fail")
     plt.close(fig)
 
 
 def centroid(vertexes):
-    _x_list = [vertex[0] for vertex in vertexes]
-    _y_list = [vertex[1] for vertex in vertexes]
+    _x_list = [vertex [0] for vertex in vertexes]
+    _y_list = [vertex [1] for vertex in vertexes]
     _len = len(vertexes)
     _x = sum(_x_list) / _len
     _y = sum(_y_list) / _len
-    return (_x, _y)
+    return _x, _y
 
 
 def crop_and_save(image_path, image, objs, fig_num=None):
@@ -78,14 +77,14 @@ def crop_and_save(image_path, image, objs, fig_num=None):
         while len(out_tfl) < len(in_tfl):
             x = random.randint(0, image.shape[0])
             y = random.randint(0, image.shape[1])
-            point = Point(x, y)
+            point = Point(x,y)
 
             if not polygon.contains(point):
-                out_tfl += [(x, y)]
+                out_tfl += [(x,y)]
     for p in in_tfl:
-        crop_image(p[0], p[1], image_path, '1')
+        crop_image(p[0],p[1] ,image_path, '1')
     for p in out_tfl:
-        crop_image(p[0], p[1], image_path, '0')
+        crop_image(p[0],p[1] ,image_path, '0')
 
 
 def find_tfl_lights(image_path, json_path=None, fig_num=None):
@@ -100,7 +99,7 @@ def find_tfl_lights(image_path, json_path=None, fig_num=None):
     crop_and_save(image_path, image, objects, fig_num)
 
 
-def main():
+def build_dataset():
     path_png = r"C:\Users\efi\Desktop\Bootcamp\mobileye\leftImg8bit\val"
     path_json = r"C:\Users\efi\Desktop\Bootcamp\mobileye\gtFine_trainvaltest\gtFine\val"
     for folder in os.listdir(path_png):
@@ -113,7 +112,3 @@ def main():
                 if img_path.endswith('_leftImg8bit.png'):
                     json_p = json_p.replace('_leftImg8bit.png', '_gtFine_polygons.json')
                     find_tfl_lights(img_path, json_p)
-
-
-if __name__ == '__main__':
-    main()
